e5090054812eaaa6806fd1410106e2b123a7b939
ggr <ginagonzalezroundey@gmail.com>
2017-12-21 00:08:29 -0800

    basic save button functionality

--- MQP/SettingsController.swift
+++ MQP/SettingsController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsController.swift
 //  MQP
 //
 //  Created by GGR on 12/20/17.
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SettingsController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func saveSettings() {
+        //TODO: update settings
+        let mainController = storyboard?.instantiateViewController(withIdentifier: "MainController") as! MainController
+        self.present(mainController, animated:false, completion:nil)
+    }
+    
 }
-
---
+++ MQP/SymptomsController.swift
@@ -0,0 +1,29 @@
+//
+//  SymptomsController.swift
+//  MQP
+//
+//  Created by GGR on 12/20/17.
+//  Copyright © 2017 ggr. All rights reserved.
+//
+
+import UIKit
+
+class SymptomsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveSymptoms() {
+        //TODO: update symptoms
+        let mainController = storyboard?.instantiateViewController(withIdentifier: "MainController") as! MainController
+        self.present(mainController, animated:false, completion:nil)
+    }
+    
+}
